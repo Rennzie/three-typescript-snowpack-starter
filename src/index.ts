ad24af4ecef8feb36acdaf11cbcae2f1b2279a8c
Sean.Rennie <rnnsea001@gmail.com>
2021-04-20 08:07:20 +0100

    polish index.ts with textures and error handling

--- src/index.ts
+++ src/index.ts
@@ -11,58 +11,53 @@ const gui = new dat.GUI();
 
 // Canvas
 const canvas: HTMLCanvasElement | null = document.querySelector('canvas.webgl');
+if (!canvas)
+  throw Error(
+    'Could not find canvas.webgl canvas in the document. Did you use the correct selector?',
+  );
 
 // Scene
 const scene = new THREE.Scene();
 
 /**
+ * Textures
+ * - Import URL is from the `Public` folder
+ */
+const textureLoader = new THREE.TextureLoader();
+
+// Bricks Texture
+const bricksAmbientOcclusionTexture = textureLoader.load(
+  'textures/bricks/ambientOcclusion.jpg',
+);
+const bricksColorTexture = textureLoader.load('textures/bricks/color.jpg');
+const bricksNormalTexture = textureLoader.load('textures/bricks/normal.jpg');
+const bricksRoughnessTexture = textureLoader.load(
+  'textures/bricks/roughness.jpg',
+);
+
+/**
  * OBJECTS
  */
 
-// RONDAVEL
-const rondavelSizes = {
-  height: 2.2,
-  radius: 2.5,
-  doorHeight: 2,
-  windowHeight: 1,
-  windowWidth: 2,
-  roofHeight: 2,
-  roofRadius: 2.65,
-};
-
-const rondavel = new THREE.Group();
-scene.add(rondavel);
-
-const bodyMaterial = new THREE.MeshStandardMaterial({ color: 'brown' });
-const bodyGeometry = new THREE.CylinderGeometry(
-  rondavelSizes.radius,
-  rondavelSizes.radius,
-  rondavelSizes.height,
-  100,
+// Box
+const box = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshStandardMaterial({
+    map: bricksColorTexture,
+    aoMap: bricksAmbientOcclusionTexture,
+    normalMap: bricksNormalTexture,
+    roughnessMap: bricksRoughnessTexture,
+  }),
 );
-const bodyMesh = new THREE.Mesh(bodyGeometry, bodyMaterial);
+box.position.y = 1;
 
-const roofMaterial = new THREE.MeshStandardMaterial({ color: 'yelow' });
-const roofGeometry = new THREE.CylinderGeometry(
-  0,
-  rondavelSizes.roofRadius,
-  rondavelSizes.roofHeight,
-  100,
-);
-
-const roofMesh = new THREE.Mesh(roofGeometry, roofMaterial);
-
-rondavel.add(roofMesh);
+scene.add(box);
 
 // Floor
 const floor = new THREE.Mesh(
   new THREE.PlaneGeometry(20, 20),
   new THREE.MeshStandardMaterial({
-    color: 'green',
-    // map: grassColorTexture,
-    // aoMap: grassAmbientOcclusionTexture,
-    // normalMap: grassNormalTexture,
-    // roughnessMap: grassRoughnessTexture,
+    color: 0x00ff00,
   }),
 );
 floor.geometry.setAttribute(
@@ -77,18 +72,17 @@ scene.add(floor);
  * Lights
  */
 // Ambient light
-const ambientLight = new THREE.AmbientLight('#b9d5ff', 0.2);
+const ambientLight = new THREE.AmbientLight('#b9d5ff', 0.5);
 gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001);
 scene.add(ambientLight);
 
-// Directional light
-const moonLight = new THREE.DirectionalLight('#ffffff', 0.75);
-moonLight.position.set(4, 5, -2);
-gui.add(moonLight, 'intensity').min(0).max(1).step(0.001);
-gui.add(moonLight.position, 'x').min(-5).max(5).step(0.001);
-gui.add(moonLight.position, 'y').min(-5).max(5).step(0.001);
-gui.add(moonLight.position, 'z').min(-5).max(5).step(0.001);
-scene.add(moonLight);
+const directionalLight = new THREE.DirectionalLight('#ffffff', 0.75);
+directionalLight.position.set(4, 5, -2);
+gui.add(directionalLight, 'intensity').min(0).max(1).step(0.001);
+gui.add(directionalLight.position, 'x').min(-5).max(5).step(0.001);
+gui.add(directionalLight.position, 'y').min(-5).max(5).step(0.001);
+gui.add(directionalLight.position, 'z').min(-5).max(5).step(0.001);
+scene.add(directionalLight);
 
 /**
  * Sizes
@@ -139,7 +133,7 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-renderer.setClearColor(0x262837);
+renderer.setClearColor(0xffffff);
 
 /**
  * Shadows
@@ -147,8 +141,7 @@ renderer.setClearColor(0x262837);
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 floor.receiveShadow = true;
-rondavel.castShadow = true;
-moonLight.castShadow = true;
+box.castShadow = true;
 
 /**
  * Animate
@@ -157,6 +150,9 @@ const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+
+  // Animate Objects
+  box.rotation.x = Math.sin(elapsedTime);
 
   // Update controls
   controls.update();
